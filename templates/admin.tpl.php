c501d11c81941b85bb791b7fe4c1f54091c882a8
Flavio tavares <tavares.pmsg@gmail.com>
2020-06-17 12:41:56 -0300

    Iniciando Template do Painel Administrativo

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -11,8 +11,38 @@
     <title>Painel Administrativo - Site Treinamento</title>
   </head>
   <body>
-    <h1>Adminstração</h1>
-    <?php include $content ?>
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            
+            <a href="" class="navbar-brand">Admin Site Treinamento</a>
+           
+            <span class="navbar-text">
+                 Painel Administrativo - Site Treinamento
+            </span>
+
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col-2">
+                <ul class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                        <span class="nav-link text-white-50"><small>MENU</small></span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-10">
+                <?php include $content ?>   
+            </div>
+        </div>
+         
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
