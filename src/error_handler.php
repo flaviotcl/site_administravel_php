89cec46457960a2a3a1186ec6f886708d2b319b2
Flavio tavares <tavares.pmsg@gmail.com>
2020-06-17 10:04:19 -0300

    Aprimorando mensagens de Erro c/ XDEBUG

--- src/error_handler.php
+++ src/error_handler.php
@@ -5,6 +5,19 @@ function setInternalServerError($errno, $errstr,  $errfile, $errline)
     
     echo '<h3>Error</> ';
 
+    if (!DEBUG) {
+        exit;
+    }
+
+    if (is_object($errno)) {
+        $err = $errno;
+        $errno = $err->getCode();
+        $errstr = $err->getMessage();
+        $errfile = $err->getFile();
+        $errline = $err->getLine();
+    }
+
+    echo '<span style="font-weigth: bold; color: red">';
     switch ($errno) {
         case E_USER_ERROR:
             echo '<strong>Error</strong> ['.$errno.'] '.$errstr."<br>\n";
@@ -20,6 +33,21 @@ function setInternalServerError($errno, $errstr,  $errfile, $errline)
             echo '<strong>Unknow error Type</strong> ['.$errno.'] '.$errstr."<br>\n";
             break;
     }
+    echo '</span>';
+
+    //Aprimorando manipulação de erros.
+    echo '<ul>';
+    foreach (debug_backtrace() as $error) {
+        if (!empty($error['file'])) {
+            echo '<li>';
+            echo $error['file'] . ':';
+            echo $error['line'];
+            echo '</li>';
+        }
+    }
+    echo '</ul>';
+
+
     exit;
 }
 
