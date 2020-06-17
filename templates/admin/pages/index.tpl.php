bb6d6c3632604206fa445f9755c2ac60d344e32e
Flavio tavares <tavares.pmsg@gmail.com>
2020-06-17 15:41:48 -0300

    Template de Listagem de Página

--- templates/admin/pages/index.tpl.php
+++ templates/admin/pages/index.tpl.php
@@ -1,4 +1,23 @@
-<h3>Administração de Páginas</h3>
-//Listando ...
+<h3 class="mb-5">Administração de Páginas</h3>
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>
+                <a href="/admin/pages/1">Página Inicial</a>
+            </td>
+            <td class="text-right">
+                <a href="/admin/pages/1" class="btn btn-primary btn-sm">View</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
-<a href="/admin">Ir p/ HOME</a>
+<a href="/admin/pages/create" class="btn btn-secondary">NEW</a>
