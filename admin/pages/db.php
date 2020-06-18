07ea4d68cb11cb6a515b6ecf7a3d46ecc5e1604d
Flavio Tavares <flaviotcl@gmail.com>
2020-06-18 15:13:47 -0300

    Criando funções p/integrar com o Banco de Dados

---
+++ admin/pages/db.php
@@ -0,0 +1,21 @@
+<?php
+
+$pages_all = function(){
+    //Listar todas as Páginas
+};
+
+$pages_one = function($id){
+    //Buscar uma única Página.
+};
+
+$pages_create = function(){
+    //Cadastrar uma Página.
+};
+
+$pages_edit = function($id){
+    //Atualizar uma página.
+};
+
+$pages_delete = function($id){
+    //Remover uma Página.
+};
