8523711a5904c05f5daae31fea7e4b6ece9ba48a
Flavio tavares <tavares.pmsg@gmail.com>
2020-06-19 16:03:25 -0300

    Cadastrando Registro

--- admin/pages/db.php
+++ admin/pages/db.php
@@ -1,5 +1,17 @@
 <?php
+function pages_get_data ($redirectOnError) {
+    $title = filter_input(INPUT_POST, 'title');
+    $url = filter_input(INPUT_POST, 'url');
+    $body = filter_input(INPUT_POST, 'body');
 
+    if (!$title) {
+        flash('Informe o campo de título', 'error');
+        header('location: ' . $redirectOnError);
+        die();
+    }
+
+    return compact('title', 'body', 'url');
+}
 $pages_all = function() use ($conn){
     $result = $conn->query('SELECT * FROM pages');
     return $result->fetch_all(MYSQLI_ASSOC);
@@ -9,9 +21,18 @@ $pages_one = function($id){
     //Buscar uma única Página.
 };
 
-$pages_create = function(){
+$pages_create = function() use ($conn){
     //Cadastrar uma Página.
-    flash('Criou registro com Sucesso', 'success');
+    $data = pages_get_data('/admin/pages/create');
+
+    $sql = 'INSERT INTO pages (title, body, url, updated, created) VALUES (?, ?, ?, NOW(), NOW())';
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('sss', $data['title'], $data['body'], $data['url']);
+
+    flash('Criou registro com sucesso', 'success');
+
+    return $stmt->execute();
 };
 
 $pages_edit = function($id){
