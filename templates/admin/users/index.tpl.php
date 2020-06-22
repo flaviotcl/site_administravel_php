9cbcfafb32c3fbf8fb0dee29027e27c85966664e
Flavio Tavares <flaviotcl@gmail.com>
2020-06-22 09:32:15 -0300

    Template de Listagem de Usuarios

--- templates/admin/users/index.tpl.php
+++ templates/admin/users/index.tpl.php
@@ -1,1 +1,22 @@
-//LIST USERS
+<h3 class="mb-5">Administração de Usuários</h3>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>flavio@gmail.com</td>
+            <td class="text-right">
+                <a href="/admin/users/1" class="btn btn-primary btn-sm">ver</a>
+            </td>
+        </tr>   
+    </tbody>
+</table>
+
+<a href="/admin/users/create" class="btn btn-secondary btn-sm">NEW</a>
