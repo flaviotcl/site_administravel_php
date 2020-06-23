12ea679ba02066eec73674bf8419990c1c5f818e
Flavio tavares <tavares.pmsg@gmail.com>
2020-06-23 14:26:59 -0300

    Criando Login

--- admin/auth/routes.php
+++ admin/auth/routes.php
@@ -1,8 +1,30 @@
 <?php
 
+include __DIR__.'/db.php';
 
 if ( resolve('/admin/auth/login'))
 {
-   
+   if ($_SERVER['REQUEST_METHOD'] === 'POST')
+   {
+       $password = filter_input(INPUT_POST, 'password') ?? null;
+       $email = filter_input(INPUT_POST, 'email') ?? null;
+       if (!$password || !$email)
+       {
+           flash('Digite seu Email/Senha !','error');
+           header('location: /admin/auth/login');
+           exit;
+       }
+       if($login())
+       {
+           flash('Login realizado com Sucesso !','success');
+           header('location: /admin');
+       }else
+       {
+           flash('Email/Senha inválidos !','error');
+           header('location: /admin/auth/login');
+           exit;
+       }
+       
+   }
    render('admin/auth/login','admin/login');
 }
--- templates/admin/auth/login.tpl.php
+++ templates/admin/auth/login.tpl.php
@@ -4,11 +4,11 @@
         <form method="post">
             <div class="form-group">
                 <label for="authLogin">Email</label>
-                <input type="email" class="form-control" name="email" id="authLogin" placeholder="Seu email">
+                <input type="email" class="form-control" name="email" id="authLogin" placeholder="Seu email" >
             </div>
             <div class="form-group">
                 <label for="authPassword">Senha</label>
-                <input type="password" class="form-control" name="password" id="authLogin" placeholder="Sua senha">
+                <input type="password" class="form-control" name="password" id="authLogin" placeholder="Sua senha" >
             </div>
             <input type="submit" value="Acessar" class="btn btn-primary btn-lg">
         </form>
